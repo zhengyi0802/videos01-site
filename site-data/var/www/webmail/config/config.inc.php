cafd04425087976c1316943f36a29603a17a649e
Chevy Lin <chevylin0802@gmail.com>
2020-03-12 03:42:20 +0000

    update

--- site-data/var/www/webmail/config/config.inc.php
+++ site-data/var/www/webmail/config/config.inc.php
@@ -11,10 +11,7 @@
 // For examples see http://pear.php.net/manual/en/package.database.mdb2.intro-dsn.php
 // NOTE: for SQLite use absolute path (Linux): 'sqlite:////full/path/to/sqlite.db?mode=0646'
 //       or (Windows): 'sqlite:///C:/full/path/to/sqlite.db'
-$config['db_dsnw'] = 'mysql://roundcubedb:password@localhost/roundcubedb';
-
-// Syslog ident string to use, if using the 'syslog' log driver.
-$config['syslog_id'] = 'webmail';
+$config['db_dsnw'] = 'mysql://roundcube:mundi8888@localhost/roundcubedb';
 
 // ----------------------------------
 // IMAP
@@ -32,47 +29,18 @@ $config['syslog_id'] = 'webmail';
 // WARNING: After hostname change update of mail_host column in users table is
 //          required to match old user data records with the new host.
 $config['default_host'] = 'localhost';
-$config['default_port'] = 143;
-$config['imap_auth_type'] = 'PLAIN';
-
-$config['smtp_server'] = 'localhost';
-$config['smtp_port'] = 587;
-$config['smtp_auth_type'] = 'PLAIN';
-
-// SMTP username (if required) if you use %u as the username Roundcube
-// will use the current username for login
-$config['smtp_user'] = '%u';
-
-// SMTP password (if required) if you use %p as the password Roundcube
-// will use the current user's password for login
-$config['smtp_pass'] = '%p';
 
 // provide an URL where a user can get support for this Roundcube installation
 // PLEASE DO NOT LINK TO THE ROUNDCUBE.NET WEBSITE HERE!
 $config['support_url'] = '';
 
-// check client IP in session authorization
-$config['ip_check'] = true;
-
 // This key is used for encrypting purposes, like storing of imap password
 // in the session. For historical reasons it's called DES_key, but it's used
 // with any configured cipher_method (see below).
-$config['des_key'] = 'HxT4Nm7fcHZpZIQAX6AvuGtR';
-
-// Automatically add this domain to user names for login
-// Only for IMAP servers that require full e-mail addresses for login
-// Specify an array with 'host' => 'domain' values to support multiple hosts
-// Supported replacement variables:
-// %h - user's IMAP hostname
-// %n - hostname ($_SERVER['SERVER_NAME'])
-// %t - hostname without the first part
-// %d - domain (http hostname $_SERVER['HTTP_HOST'] without the first part)
-// %z - IMAP domain (IMAP hostname without the first part)
-// For example %n = mail.domain.tld, %t = domain.tld
-$config['username_domain'] = 'mundi-tv.tk';
+$config['des_key'] = 'GfJx3kZK58jFPEliLFmIn6Or';
 
 // Name your service. This is displayed on the login screen and in the window title
-$config['product_name'] = '朕臨科技郵件信箱';
+$config['product_name'] = '朕臨科技電子信箱';
 
 // ----------------------------------
 // PLUGINS
@@ -80,9 +48,20 @@ $config['product_name'] = '朕臨科技郵件信箱';
 // List of active plugins (in plugins/ directory)
 $config['plugins'] = array();
 
-// the default locale setting (leave empty for auto-detection)
-// RFC1766 formatted language name like en_US, de_DE, de_CH, fr_FR, pt_BR
-$config['language'] = 'zh_TW';
+// store draft message is this mailbox
+// leave blank if draft messages should not be stored
+// NOTE: Use folder names with namespace prefix (INBOX. on Courier-IMAP)
+$config['drafts_mbox'] = '草稿';
+
+// store sent message is this mailbox
+// leave blank if sent messages should not be stored
+// NOTE: Use folder names with namespace prefix (INBOX. on Courier-IMAP)
+$config['sent_mbox'] = '寄件匣';
+
+// move messages to this folder when deleting them
+// leave blank if they should be deleted directly
+// NOTE: Use folder names with namespace prefix (INBOX. on Courier-IMAP)
+$config['trash_mbox'] = '回收筒';
 
 // Set the spell checking engine. Possible values:
 // - 'googie'  - the default (also used for connecting to Nox Spell Server, see 'spellcheck_uri' setting)
@@ -94,4 +73,3 @@ $config['language'] = 'zh_TW';
 // You can connect to any other googie-compliant service by setting 'spellcheck_uri' accordingly.
 $config['spellcheck_engine'] = 'pspell';
 
-$rcmail_config['plugins'] = array('managesieve');
